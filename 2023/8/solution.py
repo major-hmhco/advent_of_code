669dd78948f1bc8f7bf0beb2b3eaa8cedd2e896e
Simon Major <Simon.Major@hmhco.com>
2023-12-08 09:21:55 +0000

    Day8 pt2 - nice!

--- 2023/8/solution.py
+++ 2023/8/solution.py
@@ -22,3 +22,21 @@ while not node == 'ZZZ':
 print(f"Pt1: {steps}")
 
 
+#### pt2 
+def solver(start):
+    steps = 0
+    node = start
+    while not node.endswith('Z'):
+        direction = instrs[steps % len(instrs)]
+        node = nodes[node][0 if direction == 'L' else 1]
+        steps += 1
+    return steps 
+
+## this is a lowest common multiple problem - we have a stdlib for that!
+import math
+answer = 1
+for start in nodes: 
+    if start.endswith('A'):
+        answer = math.lcm(answer, solver(start))
+print(f"Pt2: {answer}") 
+
