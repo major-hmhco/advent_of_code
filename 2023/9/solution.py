957a795fa660c238b0461602c83736852624b790
Simon Major <Simon.Major@hmhco.com>
2023-12-09 13:00:23 +0000

    Remove needless prints

--- 2023/9/solution.py
+++ 2023/9/solution.py
@@ -7,11 +7,9 @@ running = 0
 for line in ipt:
     nums = [int(x) for x in line.split()] 
     delta = [nums[n] - nums[n-1] for n in range(1, len(nums))]
-    print(delta)
     deltas = [delta]
     while not all([delta[n] == 0 for n in range(0, len(delta))]):
         delta = [delta[n] - delta[n-1] for n in range(1, len(delta))]
-        print(delta)
         deltas.append(delta)
     for n in range(len(deltas) -1, 0, -1):
         deltas[n-1].append(deltas[n-1][-1] + deltas[n][-1])
@@ -27,11 +25,9 @@ running = 0
 for line in ipt:
     nums = list(reversed([int(x) for x in line.split()]))
     delta = [nums[n] - nums[n-1] for n in range(1, len(nums))]
-    print(delta)
     deltas = [delta]
     while not all([delta[n] == 0 for n in range(0, len(delta))]):
         delta = [delta[n] - delta[n-1] for n in range(1, len(delta))]
-        print(delta)
         deltas.append(delta)
     for n in range(len(deltas) -1, 0, -1):
         deltas[n-1].append(deltas[n-1][-1] + deltas[n][-1])
